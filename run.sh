1d62448761751ce4e6523654a00ef7f5d8df6e28
cvicens <carlos.vicens.alonso@gmail.com>
2023-10-02 09:35:01 +0200

    local dev

---
+++ run.sh
@@ -0,0 +1,5 @@
+export MONGODB_SERVER_HOST=mongodb
+export MONGODB_DATABASE=parksapp
+export MONGODB_USER=parksapp
+export MONGODB_PASSWORD=keepsafe
+mvn spring-boot:run -Dspring.profiles.active=local
